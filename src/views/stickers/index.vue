0707c51709157186412db15a83ee6b299ba4ab73
tianyao <ty18710388929@163.com>
2022-04-12 14:40:01 +0800

    添加了主内容区的左侧列表组件、以及左侧列表组件和父组件的交互

--- src/views/stickers/index.vue
+++ src/views/stickers/index.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="stickers">
-    <h1>This is an stickers page{{msg}}</h1>
+    <Lside @select="select" :menuData="menuData"></Lside>
+    <div>选择了{{ lsideSelect }}</div>
   </div>
 </template>
 
 <script>
+import Lside from '@/components/Lside'
 
 export default {
   name: 'StickerView',
   data() {
     return {
-      msg: '贴纸',
+      lsideSelect: '',
+      menuData: [
+        {
+          index: '1',
+          key:'My',
+          name: '我的收藏'
+        }, {
+          index: '2',
+          key:'Hot',
+          name: '热门'
+        }, {
+          index: '3',
+          key:'Funny',
+          name: '搞笑'
+        }, {
+          index: '4',
+          key:'Life',
+          name: '生活'
+        }]
+    }
+  },
+  components: {
+    Lside
+  },
+  methods: {
+    select(data) {
+      this.lsideSelect = data
     }
   }
 }
